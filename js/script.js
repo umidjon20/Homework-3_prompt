dbafd2f3c23762034869fa4fa3924eeb028e7440
Umid Amirkhulov <uamirqulov5@gmail.com>
2023-01-15 22:52:01 -0700

    learn js prompt

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let box = 'Welcome to future..'
+console.log(box)
+let namePrompt = prompt('What is your name ?')
+let surnamePrompt = prompt('What is your surname?')
+let agePrompt = prompt('How old are you?')
+ let user = {
+    name: namePrompt,
+    surname: surnamePrompt,
+    age: agePrompt
+ }
+ console.log(user.name)
+//  console.log(user.surname)
+//  console.log(user.age)
+ 
